5febbfa82cda81337dd17a0cef81f32179933b0d
Ayllah <32244294+Ayllah@users.noreply.github.com>
2020-04-18 15:48:08 -0300

    Mudanças para fazer o display realizadas

--- src/FieldInfo.cpp
+++ src/FieldInfo.cpp
@@ -1,46 +1,59 @@
 #include "FieldInfo.h"
-//#include <math.h>
 
 Fields::Fields(std::ifstream& f, u2 fieldsCount ,ConstantPool constantPool):
 std::vector<FieldInfo*>(fieldsCount){ 
 	for(u2 i = 0; i < size(); i++){
 		at(i)= new FieldInfo();
-		at(i)->accessFlags = r2(f);
+		at(i)->accessFlags.set(r2(f));
 		at(i)->nameIndex = r2(f);
 		at(i)->descriptorIndex = r2(f);
 		at(i)->attributesCount = r2(f);		
 		at(i)->attributes = new Attributes(f, at(i)->attributesCount, constantPool);
+		at(i)->init(constantPool);
 	}
 }
 
+void FieldInfo::init(ConstantPool& cp)
+{
+    name = cp.getUtf8Str(nameIndex - 1);
+    descriptor = cp.getUtf8Str(descriptorIndex - 1);
+}
 
-void Fields::printFields(){
-	std::cout << "__________________Fields__________________" << std::endl;
-	for(u2 i = 0; i < size(); i++){
-		std::cout << "accessFlags....: "   <<at(i)->accessFlags << std::endl;
-		if(at(i)->accessFlags == ACC_PUBLIC)
-			std::cout << "acessFlags.....: ACC_PUBLIC"  << std::endl;
-		else if(at(i)->accessFlags == ACC_PRIVATE)
-			std::cout << "acessFlags.....: ACC_PRIVATE"  << std::endl;
-		else if(at(i)->accessFlags ==ACC_PROTECTED)
-			std::cout << "acessFlags.....: ACC_PROTECTED"  << std::endl;
-		else if(at(i)->accessFlags ==ACC_STATIC)
-			std::cout << "acessFlags.....: ACC_STATIC"  << std::endl;
-		else if(at(i)->accessFlags ==ACC_FINAL)
-			std::cout << "acessFlags.....: ACC_FINAL"  << std::endl;
-		else if(at(i)->accessFlags ==ACC_VOLATILE)
-			std::cout << "acessFlags.....: ACC_VOLATILE"  << std::endl;
-		else if(at(i)->accessFlags ==ACC_TRANSIENT)
-			std::cout << "acessFlags.....: ACC_TRANSIENT"  << std::endl; 
-		else if(at(i)->accessFlags ==ACC_SYNTHETIC)
-			std::cout << "acessFlags.....: ACC_SYNTHETIC"  << std::endl;
-		else if(at(i)->accessFlags ==ACC_ENUM)
-			std::cout << "acessFlags.....: ACC_ENUM"  << std::endl;
-		
-		std::cout << "nameIndex......: "  << at(i)->nameIndex << std::endl;
-		std::cout << "descriptorIndex: "  << at(i)->descriptorIndex << std::endl;
-		std::cout << "attributesCount: "  << at(i)->attributesCount << std::endl;
-		//std::cout << "attributes.....: "  << at(i)->attributes << std::endl;
-		std::cout << "................................................."<< std::endl;
-	}
+void FieldInfo::print(std::ostream& out, ConstantPool& cp)
+{
+    out << std::endl << ".................................................." << std::endl;
+    out << "(Field)...............: ";
+    accessFlags.print(out);
+    printName(out);
+    printDescriptor(out);
+    out << "Attributes count.......: " << attributesCount << std::endl;
+    out << "Attributes of " << name << ":" << std::endl;
+    attributes->print(out, cp);
+}
+
+void FieldInfo::printName(std::ostream &out)
+{
+    out << "Name index.............: ";
+    out << std::hex << nameIndex << " " + name << std::dec << std::endl;
+}
+
+void FieldInfo::printDescriptor(std::ostream &out)
+{
+    out << "Descriptor index.......: ";
+    out << std::hex << descriptorIndex << " " + descriptor << std::dec << std::endl;
+}
+
+void Fields::print(std::ostream& out, ConstantPool cpt) {
+    if (size() > 0)
+    {
+        out << std::endl
+            << std::endl
+            << "__________________ Fields __________________"
+            << std::endl
+            << std::endl;
+    }
+    for (auto field : *this)
+    {
+        field->print(out, cpt);
+    }
 }
