b3efc059180299aff3219c69261d2cedfcf7827f
Ayllah <32244294+Ayllah@users.noreply.github.com>
2020-10-24 21:37:04 -0300

    Operand Stack adicionado

---
+++ include/OperandStack.h
@@ -0,0 +1,40 @@
+#ifndef OPERAND_STACK_H
+#define OPERAND_STACK_H
+
+#include <stack>  
+#include "Slot.h"
+#include "Utils.h"
+
+class Operands : public std::stack<Slot>{
+public:
+	Operands() {}
+
+	/*!
+     \brief Pega o valor do slot, passa para float e retira da pilha.
+   */
+	float popFloat();
+
+	/*!
+     \brief Pega o valor do slot, passa para double e retira da pilha.
+   */
+	double popDouble();
+
+	/*!
+     \brief Pega o valor do slot, passa para long e retira da pilha.
+   */
+	long popLong();
+
+	/*!
+     \brief Pega o valor do slot, passa para int e retira da pilha.
+   */
+	int popInt();
+
+	/*!
+     \brief Pega o valor do slot, passa para bool e retira da pilha.
+   */
+	bool popBool();
+
+
+};
+
+#endif //OPERAND_STACK_H
