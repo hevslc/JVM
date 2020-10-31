1cb459f31dd21590c9503134cd359e3d89d3cdd1
patrickvrb <patrickvrb@gmail.com>
2020-10-31 12:38:22 -0300

    Classe e Header para vector de variáveis locais com os respectivos métodos

---
+++ include/Variables.h
@@ -0,0 +1,35 @@
+#ifndef _VARIABLES_HPP_
+#define _VARIABLES_HPP_
+
+#include <vector>
+#include "Definitions.h"
+#include "Slot.h"
+
+class Variables : public std::vector<Slot>
+{
+public:
+    Variables(){};
+
+    /*!
+     \brief Pega o valor do slot, passa para float e retira do vetor.
+   */
+    float asFloat(int position);
+    /*!
+     \brief Pega o valor do slot, passa para double e retira do vetor.
+   */
+    double asDouble(int position);
+    /*!
+     \brief Pega o valor do slot, passa para inteiro e retira do vetor.
+   */
+    int asInt(int position);
+    /*!
+     \brief Pega o valor do slot, passa para long e retira do vetor.
+   */
+    long asLong(int position);
+    /*!
+     \brief Pega o valor do slot, passa para booleano e retira do vetor.
+   */
+    bool asBool(int position);
+};
+
+#endif
---
+++ src/Variables.cpp
@@ -0,0 +1,21 @@
+#include "Variables.h"
+
+float Variables::asFloat(int position)
+{
+}
+
+double Variables::asDouble(int position)
+{
+}
+
+int Variables::asInt(int position)
+{
+}
+
+long Variables::asLong(int position)
+{
+}
+
+bool Variables::asBool(int position)
+{
+}
