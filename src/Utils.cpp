71dfd70b84af67699fc78845fb8778e38eee18a0
Kalley Wilkerson <kalleywra@gmail.com>
2020-11-08 15:29:05 -0300

    #143 - Instruções criada Ainda faltam fazer alguns testes e verificar a criação de outras funções

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -29,6 +29,19 @@ bool getBool(u4 bytes){
 
 }
 
+short getShort(u4 bytes) {
+	uint16_t v = uint16_t(bytes);
+	return reinterpret_cast<u2&>(v);
+}
+
+int8_t getByte(u4 bytes) {
+	return bytes;
+}
+
+char getChar(u4 bytes) {
+	return bytes;
+}
+
 u2 getIndex(u1 byte1, u1 byte2) {
 	return ((u2)byte1 << 8) | byte2;
 }
